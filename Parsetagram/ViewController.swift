3e1328485ec52e2e49bca5bcfe7be744678a92cc
Avni Avdulla <avdullaa@msu.edu>
2020-10-24 17:21:42 -0400

    Initial Commit

---
+++ Parsetagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parsetagram
+//
+//  Created by Avni Avdulla on 10/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
